3f838520c30021c4568ec46822bb68524adb8511
ElonGates <59313695+VilotStar@users.noreply.github.com>
2021-11-14 15:11:08 +0000

    Not fully done but leaving for a bit so yea
    Autist just take a look and do da funni if possible

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use iced::{button, Align, Button, Column, Element, Font, Sandbox, Settings, Text};
+#[allow(unused_imports)]
+
+use iced::{
+    button, scrollable, slider, text_input, Button, Checkbox, Color,
+    Column, Container, Element, Image, Length, Radio, Row, Sandbox, Scrollable,
+    Settings, Slider, Space, Text, TextInput
+};
 
 const DEF_FONT: Font = Font::External {
     name: "Iconsolata-Regular.ttf",
@@ -6,51 +12,143 @@ const DEF_FONT: Font = Font::External {
 };
 
 pub fn main() -> iced::Result {
-    Counter::run(Settings::default())
+    Main::run(Settings::default())
 }
 
-#[derive(Default)]
-struct Counter {
-    value: String,
-    put_box: button::State,
+pub struct Main {
+    frames: Frames
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    PutInBox
-}
-
-impl Sandbox for Counter {
+impl Sandbox for Main {
     type Message = Message;
 
-    fn new() -> Self {
-        Self::default()
+    fn new() -> Main {
+        Main { 
+            frames: Frames::new()
+        }
     }
 
     fn title(&self) -> String {
-        String::from("Nozomi")
+        format!("{} - Nozomi", self.frames.title())
     }
 
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::PutInBox => {
-                self.value = String::from("human genitals");
+    fn update(&mut self, event: Message) {
+        match event {
+            Message::FrameMessage(frame_msg) => {
+                self.frames.update(frame_msg);
             }
         }
     }
 
     fn view(&mut self) -> Element<Message> {
-        Column::new()
+        let Main {
+            frames,
+            ..
+        } = self;
+
+        let controls = Row::new();
+
+        let content: Element<_> = Column::new()
+            .max_width(540)
+            .spacing(20)
             .padding(20)
-            .align_items(Align::Center)
-            .push(
-                Button::new(
-                    &mut self.put_box,
-                    Text::new("Put in da box").font(DEF_FONT),
-                )
-                .on_press(Message::PutInBox),
-            )
-            .push(Text::new(self.value.to_string()).size(50).font(DEF_FONT))
+            .push(frames.view().map(Message::FrameMessage))
+            .push(controls)
+            .into();
+
+        Container::new(content)
+            .height(Length::Fill)
+            .center_y()
             .into()
+    }
+}
+
+impl Frames {
+    fn new() -> Frames {
+        Frames {
+            frames: vec![
+                Frame::LoginFrame
+            ],
+            current: 0
+        }
+    }
+
+    fn update(&mut self, msg: FrameMessage) {
+        self.frames[self.current].update(msg);
+    }
+
+    fn view(&mut self) -> Element<FrameMessage> {
+        self.frames[self.current].view()
+    }
+
+    fn title(&self) -> &str {
+        self.frames[self.current].title()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    FrameMessage(FrameMessage)
+}
+
+struct Frames {
+    frames: Vec<Frame>,
+    current: usize
+}
+
+enum Frame {
+    LoginFrame {
+        value: String
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum FrameMessage {
+    TextInputChanged(String)
+}
+
+impl<'a> Frame {
+    fn update(&mut self, msg: FrameMessage) {
+        match msg {
+            FrameMessage::TextInputChanged(value) => {
+                if let Frame::TextInput
+            }
+        }
+    }
+
+    fn view(&mut self) -> Element<FrameMessage> {
+        match self {
+            Frame::LoginFrame { value, state } => Self::loginframe(value, state)
+        }
+        .into()
+    }
+
+    fn container(title: &str) -> Column<'a, FrameMessage> {
+        Column::new().spacing(20).push(Text::new(title).size(50))
+    }
+
+    fn title(&self) -> &str {
+        match self {
+            Frame::LoginFrame { .. } => "Login"
+        }
+    }
+
+    fn loginframe(state: &'a mut loginframe::State, value &str) -> Column<'a, FrameMessage> {
+        let loginframe = TextInput::new(
+            state,
+            "Username",
+            value,
+            FrameMessage::TextInputChanged
+        )
+        .padding(10)
+        .size(30);
+        Self::container("Login")
+            .push(
+                Text::new(if value.is_empty() {
+                    "Username"
+                } else {
+                    value
+                })
+            )
     }
 }
